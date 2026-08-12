a11a5b33700f29cc382af22ea2a7f7f8dd866bfc
Rameesha <rrnahmad2004@gmail.com>
2026-08-12 03:17:30 +0200

    Initial project setup

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+    res.json({
+        success: true,
+        message: "MisandristMeesha backend is running."
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
